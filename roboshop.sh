9feacdeaf87d9211f95240bc84758a813ba7dcbc
Maheshreddy419 <mahesh.apple62@gmail.com>
2025-05-21 22:29:59 +0530

    shell commit

--- roboshop.sh
+++ roboshop.sh
@@ -7,13 +7,15 @@ ZONE_ID="Z037075933N1X2PBQRMEY"
 DOMAIN_NAME="maheshdevops.shop"
 
 for instances in ${INSTANCES[@]}
-do 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0a290863a0dd711ac --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=test}]" --query "Instances[0].InstanceId" --output text)
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0a290863a0dd711ac --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
-    echo "$instance IP Address: $IP"
+    echo "$instance IP address: $IP"
 done
