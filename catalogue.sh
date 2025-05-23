e9634e575880af01e9cc9a2f3a36576404d812ab
Maheshreddy419 <mahesh.apple62@gmail.com>
2025-05-24 01:58:07 +0530

    shell commit

--- catalogue.sh
+++ catalogue.sh
@@ -6,14 +6,12 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
-LOGS_FOLDER="/var/logs/roboshop-logs"
+LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-$SCRIPT_DIR=$PWD
+SCRIPT_DIR=$PWD
 
-
-mkdir -p $LOG_FOLDER
+mkdir -p $LOGS_FOLDER
 echo "Script excecution started at:: $(date)" | tee -a $LOG_FILE
 
 #checking the user is root user or not
